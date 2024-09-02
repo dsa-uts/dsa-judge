b1b0bc45c0ea8472e9a6936f14a51bd1d64d2749
Takuya Mizokami <takoyaki65@users.noreply.github.com>
2024-09-02 18:59:43 +0900

    ロガーの設定を追加

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, HTTPException, UploadFile, File
 from contextlib import asynccontextmanager
-import logging
+import datetime
 from concurrent.futures import ThreadPoolExecutor, Future
 import asyncio
 from db.crud import *
@@ -9,9 +9,8 @@ from db.database import SessionLocal
 from sandbox.my_error import Error
 from judge import JudgeInfo
 
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger("uvicorn")
-
+from log.config import judge_logger
+from sandbox.execute import define_sandbox_logger
 
 class WorkerPool:
     max_workers: int
@@ -43,11 +42,11 @@ class WorkerPool:
 worker_pool = WorkerPool(max_workers=50)
 
 def process_one_judge_request(submission: SubmissionRecord) -> Error:
-    logger.info(f"JudgeInfo(submission_id={submission.id}, lecture_id={submission.lecture_id}, assignment_id={submission.assignment_id}, for_evaluation={submission.for_evaluation}) will be created...")
+    judge_logger.debug(f"JudgeInfo(submission_id={submission.id}, lecture_id={submission.lecture_id}, assignment_id={submission.assignment_id}, for_evaluation={submission.for_evaluation}) will be created...")
     judge_info = JudgeInfo(submission)
-    logger.info("START JUDGE...")
+    judge_logger.debug("START JUDGE...")
     err = judge_info.judge()
-    logger.info("END JUDGE")
+    judge_logger.debug("END JUDGE")
     
     return err
 
@@ -56,42 +55,45 @@ async def process_judge_requests():
         try:
             completed_jobrecord_list = worker_pool.collect_completed_jobs()
             for completed_jobrecord in completed_jobrecord_list:
-                logger.info(f"job: \"{completed_jobrecord[0]}\", date: {completed_jobrecord[1]}, result: {completed_jobrecord[2]}")
+                judge_logger.info(f"job: \"{completed_jobrecord[0]}\", date: {completed_jobrecord[1]}, result: {completed_jobrecord[2]}")
             with SessionLocal() as db:
                 num_available_workers = worker_pool.available_workers()
                 queued_submissions = fetch_queued_judge_and_change_status_to_running(db, num_available_workers)
             if queued_submissions:
-                logger.info(
+                judge_logger.info(
                     f"{len(queued_submissions)}件のジャッジリクエストを取得しました。"
                 )
                 # スレッドプールを使用して各ジャッジリクエストを処理
                 for submission in queued_submissions:
-                    logger.info(f"submission: {submission}")
-                    logger.info("throw judge request to thread pool...")
+                    judge_logger.info(f"submission: {submission}")
+                    judge_logger.info("throw judge request to thread pool...")
                     worker_pool.submit_job(f"submission-{submission.id}", process_one_judge_request, submission)
-            else:
-                logger.info("キューにジャッジリクエストはありません。")
+            # else:
+            #     # uvicorn_logger.info("キューにジャッジリクエストはありません。")
         except Exception as e:
             import traceback
-            logger.error(f"例外が発生しました: {type(e).__name__}: {str(e)}")
-            logger.error(f"スタックトレース:\n{traceback.format_exc()}")
-            logger.info("データベースに接続できない可能性があります。準備ができていない可能性があります。")
+            judge_logger.critical(f"例外が発生しました: {type(e).__name__}: {str(e)}")
+            judge_logger.critical(f"スタックトレース:\n{traceback.format_exc()}")
+            judge_logger.critical("データベースに接続できない可能性があります。準備ができていない可能性があります。")
 
         await asyncio.sleep(5)  # 5秒待機
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    logger.info("LIFESPAN LOGIC INITIALIZED...")
+    # sandboxのデバッグ文(どのDockerコマンドを実行しました、etc)を出力するロガーの設定
+    define_sandbox_logger(logger=judge_logger)
+    define_crud_logger(logger=judge_logger)
+    judge_logger.info("LIFESPAN LOGIC INITIALIZED...")
     task = asyncio.create_task(process_judge_requests())
     yield
     task.cancel()
-    logger.info("LIFESPAN LOGIC DEACTIVATED...")
+    judge_logger.info("LIFESPAN LOGIC DEACTIVATED...")
     # 現在実行しているジャッジリクエストを最後まで実行し、保留状態のものは破棄する
     worker_pool.executor.shutdown(wait=True, cancel_futures=True)
     completed_jobrecord_list = worker_pool.collect_completed_jobs()
     for completed_jobrecord in completed_jobrecord_list:
-        logger.info(f"job: \"{completed_jobrecord[0]}\", date: {completed_jobrecord[1]}, result: {completed_jobrecord[2]}")
+        judge_logger.info(f"job: \"{completed_jobrecord[0]}\", date: {completed_jobrecord[1]}, result: {completed_jobrecord[2]}")
     # statusをrunningにしてしまっているタスクをqueuedに戻す
     # そして途中結果を削除する
     with SessionLocal() as db:
