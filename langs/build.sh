b41eb4381a7380b96ab7593358caabcb35be44f9
Takuya Mizokami <takoyaki65@users.noreply.github.com>
2025-03-04 20:50:09 +0900

    update crun and build scripts for sandbox

--- langs/build.sh
+++ langs/build.sh
@@ -4,15 +4,14 @@ set -e
 
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
 
-# builderイメージをビルド
-docker build -t builder-watchdog -f $SCRIPT_DIR/Dockerfile.builder $SCRIPT_DIR
-
-# watchdog.cppのコンパイル
-docker run --rm -it -v $SCRIPT_DIR:/work --workdir /work builder-watchdog \
-  bash -c "g++ -o watchdog watchdog.cpp"
+# watchdogのビルドステージをビルド
+docker build -t watchdog-builder -f $SCRIPT_DIR/Dockerfile.builder $SCRIPT_DIR
 
 # GCCを使えるsandboxイメージをビルド
 docker build -t checker-lang-gcc -f $SCRIPT_DIR/Dockerfile.GCC $SCRIPT_DIR
 
 # 実行用のsandboxイメージをビルド
 docker build -t binary-runner -f $SCRIPT_DIR/Dockerfile.binary-runner $SCRIPT_DIR
+
+# ビルドステージを削除
+docker rmi watchdog-builder
--- prepare-sandbox.sh
+++ prepare-sandbox.sh
@@ -14,16 +14,9 @@ SCRIPT_DIR=$(cd $(dirname $0); pwd)
 # crunのインストール
 if ! which crun > /dev/null 2>&1; then
     echo "crunがインストールされていません。インストールを開始します。"
-    wget https://github.com/containers/crun/releases/download/1.19.1/crun-1.19.1-linux-amd64 -O /usr/local/bin/crun
-    chmod +x /usr/local/bin/crun
-    echo "crunのインストールが完了しました。"
-    # dockerの設定(デフォルトランタイムをcrunにする)
-    # /etc/dockerがない場合は作成、ある場合でもエラーにならないようにする
-    mkdir -p /etc/docker
-    cp $SCRIPT_DIR/docker-daemon.json /etc/docker/daemon.json
-
-    # dockerの再起動
-    systemctl restart docker
+    $SCRIPT_DIR/install-crun.sh
 else
     echo "crunは既にインストールされています。"
 fi
+
+$SCRIPT_DIR/langs/build.sh
